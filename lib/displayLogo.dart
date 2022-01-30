2365131cb1fed0b44e3b5d86ace0ae09da247d49
vjtrollkilla <vjsingh208000@gmail.com>
2022-01-30 18:01:00 +0300

    Add new dart logo file and start page
    added assets file which was registered to pubspec.yam then created a new file called display logo which returns a widget for the logo

---
+++ lib/displayLogo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class logo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 0, bottom: 50),
+      child: Image.asset('assets/images/WhichCar.jpeg'),
+      width: 200.0,
+      height: 200.0,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'displayLogo.dart';
 
+void main() {
+  runApp(landingPage());
+}
+
+class landingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.black87,
+          title: Container(
+              width: double.infinity,
+              child: Text(
+                'Which Car',
+                textAlign: TextAlign.center,
+              )),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[logo(), Text('HEllo')],
+          ),
+        ),
+      ),
+    );
+  }
+}
